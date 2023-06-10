974f2e861a1424a3e46c1adbdac678e1dd8f6abc
Osimitzu <angel_aocc@hotmail.com>
2023-06-10 09:37:06 -0600

    swagger added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ const postRoutes = require("./posts.routes");
 const categoriesRoutes = require("./categories.routes");
 const answersRoutes = require("./answers.routes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("../swagger.json");
+
 // recibe como parametro una instancia de expres
 const apiRoutes = (app) => {
   app.use(userRoutes);
   app.use(postRoutes);
   app.use(categoriesRoutes);
   app.use(answersRoutes);
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 };
 
 module.exports = apiRoutes;
